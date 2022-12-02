9cd2c0d5988c1b4ff331a37ce017410459e9d896
bloohk <elias.scofield@hotmail.com>
2022-12-02 08:34:43 +0100

    v2.2

--- game.py
+++ game.py
@@ -5,6 +5,8 @@ import random
 # Required
 # -----------------------------------------------------------------------------------------------------------------------------------------
 
+yesorno = False
+
 # Player variables
 player_health = 100
 
@@ -13,26 +15,61 @@ ai_name = "Computer"
 ai_health = 100
 
 # Weapons
-sword =["Sword", 20, 85, "Medium damage, high speed."]
+sword =["Sword", 24, 85, "Medium damage, fast."]
 club = ["Club", 30, 70, "Medium-high damage, medium speed"]
-bow = ["Bow", 18, 90, "Medium damage, very high speed"]
+bow = ["Bow", 20, 90, "Low-Medium damage, very fast"]
+dagger = ["Dagger", 15, 100, "Low damage, extremely fast - sure to hit"]
+mace = ["Mace", 38, 60, "High damage, slow"]
+fish = ["Massive trout", 45, 50, "Massive damage, very slow"]
 
-weapons = [sword, bow, club] # For weapon choice
+weapons = [sword, bow, club, dagger, mace, fish] # For weapon choice
 
 class Player:
     def __init__(self, name, health):
         self.name = name
         self.health = health
 
-    def dmg_calc(self, wp_stats, enemy):
-        print(enemy.name+" used "+str(wp_stats[0])+"!")
-        dmg_value = wp_stats[1] =+ float(wp_stats[1]*random.uniform(0.95, 1.3))
+    def dmg_calc(self, wpn_stats, enemy):
+        print("\n"+enemy.name+" used "+str(wpn_stats[0])+"!")
+        dmg_value = wpn_stats[1] =+ float(wpn_stats[1]*random.uniform(0.9, 1.3))
         dmg_value = int(round(dmg_value))
-        if random.randint(0, 100) <= wp_stats[2]:
+        if random.randint(0, 100) <= wpn_stats[2]:
             self.health -= dmg_value
             print(self.name +" took "+str(dmg_value)+" damage!\n"+self.name+" now has "+str(self.health)+" health left.")
         else:
             print(enemy.name+" missed! No damage.")
+        print("------------------------")
+
+    def wpn_choice(self, wp_list):
+        #wpn_chosen = []
+        print("Choose your weapon!")
+        wp1 = random.choice(wp_list)
+        while True:
+            wp2 = random.choice(wp_list)
+            if wp2 != wp1:
+                break
+            else:
+                pass
+        while True:
+            wp3 = random.choice(wp_list)
+            if wp3 == wp1:
+                pass
+            elif wp3 == wp2:
+                pass
+            else:
+                break
+
+        print("1: "+wp1[0]+" - "+wp1[3]+"\n2: "+wp2[0]+" - "+wp2[3]+"\n3: "+wp3[0]+" - "+wp3[3])
+        wp_choice = input("Choice: ")
+        if wp_choice == "1":
+            return wp1
+        elif wp_choice == "2":
+            return wp2
+        elif wp_choice == "3":
+            return wp3
+        
+
+        #return input("Choice (number): ")
 
     def ai_turn(self):
         return random.choice(weapons)
@@ -42,6 +79,7 @@ class Player:
 # -----------------------------------------------------------------------------------------------------------------------------------------
 
 def play_round(player, enemy):
+    wp_list = []
     if player.name == ai_name: # If player is computer
         print("\n"+player.name+"'s turn!\n")
         sleep(2)
@@ -51,21 +89,7 @@ def play_round(player, enemy):
     else: # If player is player lol
         print("\n"+player.name+"'s turn!\n")
         sleep(2)
-        print("Choose your weapon!")
-        wp1 = random.choice(weapons)
-        wp2 = random.choice(weapons)
-        wp3 = random.choice(weapons)
-        print("1: "+wp1[0]+" - "+wp1[3]+"\n2: "+wp2[0]+" - "+wp2[3]+"\n3: "+wp3[0]+" - "+wp3[3])
-
-        wp_choice = input("Choice: ")
-        if wp_choice == "1":
-            enemy.dmg_calc(wp1, player)
-        elif wp_choice == "2":
-            #print(player.name+" used "+str(wp2[0])+"!")
-            enemy.dmg_calc(wp2, player)
-        elif wp_choice == "3":
-            #print(player.name+" used "+str(wp3[0])+"!")
-            enemy.dmg_calc(wp3, player)
+        enemy.dmg_calc(player.wpn_choice(weapons), player)
 
 # -----------------------------------------------------------------------------------------------------------------------------------------
 # Startup
@@ -77,23 +101,24 @@ def game():
 
     # Round shuffling
     round_count = 1
-    print ("---- ROUND "+str(round_count)+" START! ----")
+    print ("---- ! GAME START ! ----")
     while True:
         play_round(plc, ai)
         if ai.health <= 0:
             print("You won!")
-            round_count += 1
             input()
             break
-        
+        round_count += 1
+
         play_round(ai, plc)
         if plc.health <= 0:
             print("You died.")
-            round_count += 1
-            input()
+            input("")
             break
+        round_count += 1
 
-print("Console Turn Based Fight v2.0\n")
+
+print("Console Turn Based Fight v2.2\n")
 sleep(1)
 game()
 
